14c268f144a5c057630eedb80f0e423d32395e9c
Sonaliyadav17 <144614845+Sonaliyadav17@users.noreply.github.com>
2025-03-29 16:50:17 +0000

    1

---
+++ sort.java/select.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class select {
+    
+    public static void sec(int arr[]) {
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3, 1, 5, 4, 2};
+
+        sec(arr); 
+
+        System.out.println(Arrays.toString(arr)); 
+    }
+}
